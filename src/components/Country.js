4bb660f5d77e989f12073069e5234403327b644a
Emily Daykin <emilydaykin31@gmail.com>
2022-05-05 23:09:09 +0200

    updated single country page background and styling

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { countryAPI } from '../lib/api';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faMountain,
+  faTree,
+  faCloudBolt,
+  faPaw,
+  faPersonHiking
+} from '@fortawesome/free-solid-svg-icons';
 
 const Country = () => {
   const { id } = useParams();
@@ -21,39 +29,75 @@ const Country = () => {
   return (
     <section className='country-section'>
       <div className='country-wrapper'>
-        <h1>{id}</h1>
+        <h1 className='country-name'>{id}</h1>
         {!countryData ? (
           <p>Loading Data for {id}...</p>
         ) : (
           <div className='facts-wrapper'>
-            <div className='fast-facts'>
+            <div className='facts fast-facts'>
               <h3>Fast Facts</h3>
               <p>
-                <span>Capital:</span> {countryData.capital}
+                <span className='data-category'>Capital:</span> {countryData.capital}
               </p>
               <p>
-                <span>Region:</span> {countryData.region}
+                <span className='data-category'>Region:</span> {countryData.region}
               </p>
               <p>
-                <span>Currency:</span> {countryData.currency.name}
+                <span className='data-category'>Currency:</span> {countryData.currency.name}
               </p>
               <p>
-                <span>GDP/capita:</span> US$&nbsp;
+                <span className='data-category'>GDP/capita:</span> US$&nbsp;
                 {Number(countryData.gdp_per_capita).toLocaleString(undefined, {
                   minimumFractionDigits: 2
                 })}
               </p>
               <p>
-                <span>Internet Users:</span> {countryData.internet_users}%
+                <span className='data-category'>Internet Users:</span> {countryData.internet_users}%
               </p>
             </div>
-            <div className='co2-facts'>
+            <div className='facts all-green-facts'>
               <h3>Green Facts</h3>
-              <p>Co2 emissions: {countryData.co2_emissions}</p>
-              <p>Forested Area: {countryData.forested_area}%</p>
-              <p>Surface Area: {countryData.surface_area}</p>
-              <p>Threatened Species: {countryData.threatened_species}</p>
-              <p>Tourists: {countryData.tourists}</p>
+              <p>
+                <FontAwesomeIcon icon={faCloudBolt} />
+                <span className='data-category'>
+                  CO<span className='subscript'>2</span> emissions:
+                </span>{' '}
+                {countryData.co2_emissions.toLocaleString()} mt
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faTree} />
+                <span className='data-category'>Forested Area:</span> {countryData.forested_area}%
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faMountain} />
+                <span className='data-category'>Surface Area:</span>{' '}
+                {countryData.surface_area.toLocaleString()} km
+                <span className='superscript'>2</span>
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faPaw} />
+                <span className='data-category'>Threatened Species (n):</span>{' '}
+                {countryData.threatened_species.toLocaleString()}
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faPersonHiking} />
+                <span className='data-category'>Tourists ('000s):</span>{' '}
+                {countryData.tourists.toLocaleString()}
+              </p>
+            </div>
+            <div className='green-facts-TEMP'>
+              <div className='facts green-fact'>
+                <span className='data-heading'>
+                  CO<span className='subscript'>2</span> emissions
+                </span>
+                <FontAwesomeIcon className='icon' icon={faCloudBolt} />
+                <span className='data'>{countryData.co2_emissions.toLocaleString()} mt</span>
+              </div>
+              <div className='facts green-fact'>
+                <span className='data-heading'>Forested Area</span>
+                <FontAwesomeIcon className='icon' icon={faTree} />
+                <span className='data'>{countryData.forested_area}%</span>
+              </div>
             </div>
           </div>
         )}
