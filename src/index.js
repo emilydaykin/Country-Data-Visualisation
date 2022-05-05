e5e05f5e602a004218d5d6da836161dfd15c301a
Emily Daykin <emilydaykin31@gmail.com>
2022-05-05 23:12:19 +0200

    updated main.scss and added new scss imports to index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import './styles/navbar.scss';
 import './styles/home.scss';
 import './styles/about.scss';
 import './styles/countries.scss';
+import './styles/countries-chart.scss';
 import './styles/country.scss';
+import './styles/bubble.scss';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
