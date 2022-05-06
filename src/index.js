44a09c5e851cd2bb6f19852af2365714ce8f1451
Emily Daykin <emilydaykin31@gmail.com>
2022-05-06 22:43:18 +0200

    added loading component for country and countries pages

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './styles/countries.scss';
 import './styles/countries-chart.scss';
 import './styles/country.scss';
 import './styles/bubble.scss';
+import './styles/loading.scss';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
