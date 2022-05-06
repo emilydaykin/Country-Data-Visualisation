3257a0e042cad0e3f971e10d0a5b158c6ca3f5d9
Emily Daykin <emilydaykin31@gmail.com>
2022-05-06 22:42:52 +0200

    added loading component for country and countries pages

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL = 'https://api.api-ninjas.com/v1/country?';
 
 export const countryAPI = async (country) => {
-  console.log(`Fetching data for ${country}...`);
+  // console.log(`Fetching data for ${country}...`);
   const res = await axios({
     method: 'get',
     url: `${baseURL}name=${country}`,
