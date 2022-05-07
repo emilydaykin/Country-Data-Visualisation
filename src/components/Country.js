2795d026cf39277d4fcf7508311987646f8b8772
Emily Daykin <emilydaykin31@gmail.com>
2022-05-08 00:15:17 +0200

    added more (non-green) api data to single country page

--- src/components/Country.js
+++ src/components/Country.js
@@ -45,7 +45,11 @@ const Country = () => {
       return data ? `${data.name} (${data.code})` : 'No Data';
       // } else if (data === countryData.exports) {
       //   return data ? data.toLocaleString() : 'No Data';
-    } else if (data === countryData.gdp || data === countryData.exports) {
+    } else if (
+      data === countryData.gdp ||
+      data === countryData.exports ||
+      data === countryData.imports
+    ) {
       if (!data) {
         return 'No Data';
       } else if (data.toString().length >= 7) {
@@ -107,71 +111,162 @@ const Country = () => {
             </div>
 
             <div className='fast-facts-container'>
-              <div className='facts general-facts'>
-                <h3>Geography</h3>
-                <div className='fast-facts'>
-                  <p>
-                    <span className='data-category'>Capital:</span>&nbsp;
-                    {showFastFact(countryData.capital)}
-                  </p>
-                  <p>
-                    <span className='data-category'>Region:</span>{' '}
-                    {showFastFact(countryData.region)}
-                  </p>
-                  <p>
-                    <span className='data-category'>Population:</span>&nbsp;
-                    {showFastFact(countryData.population)}
-                  </p>
-                  <p>
-                    <span className='data-category'>Refugees:</span>&nbsp;
-                    {showFastFact(countryData.refugees)}
-                  </p>
-                  <p>
-                    <span className='data-category'>Population Growth (annual change):</span>&nbsp;
-                    {showFastFact(countryData.pop_growth)}
-                  </p>
-                  <p>
-                    <span className='data-category'>
-                      Population Density (per km<span className='superscript--small'>2</span>):
-                    </span>
-                    &nbsp;
-                    {showFastFact(countryData.pop_density)}
-                  </p>
+              <div className='top-facts'>
+                <div className='facts general-facts-top'>
+                  <h3>Geography</h3>
+                  <div className='fast-facts'>
+                    <p>
+                      <span className='data-category'>Capital:</span>&nbsp;
+                      {showFastFact(countryData.capital)}
+                    </p>
+                    <p>
+                      <span className='data-category'>Region:</span>{' '}
+                      {showFastFact(countryData.region)}
+                    </p>
+                    <p>
+                      <span className='data-category'>Population:</span>&nbsp;
+                      {showFastFact(countryData.population)}
+                    </p>
+                    <p>
+                      <span className='data-category'>Population Growth:</span>
+                      &nbsp;
+                      {showFastFact(countryData.pop_growth)}
+                    </p>
+                    <p>
+                      <span className='data-category'>Urban Population (%):</span>&nbsp;
+                      {showFastFact(countryData.urban_population)}
+                    </p>
+                    <p>
+                      <span className='data-category'>Refugees:</span>&nbsp;
+                      {showFastFact(countryData.refugees)}
+                    </p>
+                    <p>
+                      <span className='data-category'>
+                        Population Density (per km<span className='superscript--small'>2</span>):
+                      </span>
+                      &nbsp;
+                      {showFastFact(countryData.pop_density)}
+                    </p>
+                    <p>
+                      <span className='data-category'>Sex Ratio:</span>
+                      &nbsp;
+                      {showFastFact(countryData.sex_ratio)}
+                    </p>
+                  </div>
+                </div>
+                <div className='facts general-facts-top'>
+                  <h3>Economy</h3>
+                  <div className='fast-facts'>
+                    <p>
+                      <span className='data-category'>Currency:</span>&nbsp;
+                      {showFastFact(countryData.currency)}
+                    </p>
+                    <p>
+                      <span className='data-category'>GDP (US$):</span>&nbsp;
+                      {showFastFact(countryData.gdp)}
+                    </p>
+                    <p>
+                      <span className='data-category'>GDP/capita (US$):</span>&nbsp;
+                      {showFastFact(countryData.gdp_per_capita)}
+                    </p>
+                    <p>
+                      <span className='data-category'>Internet Users (%):</span>&nbsp;
+                      {showFastFact(countryData.internet_users)}
+                    </p>
+                    <p>
+                      <span className='data-category'>Unemployment (%):</span>
+                      &nbsp;
+                      {showFastFact(countryData.unemployment)}
+                    </p>
+                    <p>
+                      <span className='data-category'>
+                        Homicide Rate (per 100,000 inhabitants):
+                      </span>
+                      &nbsp;
+                      {showFastFact(countryData.homicide_rate)}
+                    </p>
+                    <p>
+                      <span className='data-category'>Exports (US$):</span>
+                      &nbsp;
+                      {showFastFact(countryData.exports)}
+                    </p>
+                    <p>
+                      <span className='data-category'>Imports (US$):</span>
+                      &nbsp;
+                      {showFastFact(countryData.imports)}
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className='facts general-facts'>
-                <h3>Economy</h3>
+              <div className='facts general-facts-bottom'>
+                <h3>Education & Health (PIE & BAR CHARTS)</h3>
                 <div className='fast-facts'>
-                  <p>
-                    <span className='data-category'>Currency:</span>&nbsp;
-                    {showFastFact(countryData.currency)}
-                  </p>
-                  <p>
-                    <span className='data-category'>GDP (US$):</span>&nbsp;
-                    {showFastFact(countryData.gdp)}
-                  </p>
-                  <p>
-                    <span className='data-category'>GDP/capita (US$):</span>&nbsp;
-                    {showFastFact(countryData.gdp_per_capita)}
-                  </p>
-                  <p>
-                    <span className='data-category'>Internet Users (%):</span>&nbsp;
-                    {showFastFact(countryData.internet_users)}
-                  </p>
                   <p>
                     <span className='data-category'>Infant Mortality (per 1,000 live births):</span>
                     &nbsp;
                     {showFastFact(countryData.infant_mortality)}
                   </p>
                   <p>
-                    <span className='data-category'>Homicide Rate (per 100,000 inhabitants):</span>
+                    <span className='data-category'>Fertility (births per woman):</span>
                     &nbsp;
-                    {showFastFact(countryData.homicide_rate)}
+                    {showFastFact(countryData.fertility)}
                   </p>
                   <p>
-                    <span className='data-category'>Exports (US$):</span>
+                    <span className='data-category'>Employment Agriculture (%):</span>
                     &nbsp;
-                    {showFastFact(countryData.exports)}
+                    {showFastFact(countryData.employment_agriculture)}
+                  </p>
+                  <p>
+                    <span className='data-category'>Employment Industry (%):</span>
+                    &nbsp;
+                    {showFastFact(countryData.employment_industry)}
+                  </p>
+                  <p>
+                    <span className='data-category'>Employment Services (%):</span>
+                    &nbsp;
+                    {showFastFact(countryData.employment_services)}
+                  </p>
+                  <p>
+                    <span className='data-category'>Life expectancy (female):</span>
+                    &nbsp;
+                    {showFastFact(countryData.life_expectancy_female)}
+                  </p>
+                  <p>
+                    <span className='data-category'>Life expectancy (male):</span>
+                    &nbsp;
+                    {showFastFact(countryData.life_expectancy_male)}
+                  </p>
+                  <p>
+                    <span className='data-category'>Primary school enrollment (female):</span>
+                    &nbsp;
+                    {showFastFact(countryData.primary_school_enrollment_female)}
+                  </p>
+                  <p>
+                    <span className='data-category'>Primary school enrollment (male):</span>
+                    &nbsp;
+                    {showFastFact(countryData.primary_school_enrollment_male)}
+                  </p>
+                  <p>
+                    <span className='data-category'>Secondary school enrollment (female):</span>
+                    &nbsp;
+                    {showFastFact(countryData.secondary_school_enrollment_female)}
+                  </p>
+                  <p>
+                    <span className='data-category'>Secondary school enrollment (male):</span>
+                    &nbsp;
+                    {showFastFact(countryData.secondary_school_enrollment_male)}
+                  </p>
+                  <p>
+                    <span className='data-category'>
+                      Post Secondary school enrollment (female):
+                    </span>
+                    &nbsp;
+                    {showFastFact(countryData.post_secondary_enrollment_female)}
+                  </p>
+                  <p>
+                    <span className='data-category'>Post Secondary school enrollment (male):</span>
+                    &nbsp;
+                    {showFastFact(countryData.post_secondary_enrollment_male)}
                   </p>
                 </div>
               </div>
